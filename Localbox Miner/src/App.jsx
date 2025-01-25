0c18ec32d105c430c506f1900d714b1435e5a155
pooja00617 <pooja.rnw@gmail.com>
2025-01-25 11:41:47 +0530

    Add files via upload

---
+++ Localbox Miner/src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './Componets/Form';
+// import Form from './components/Form';
+function App() {
+  return (
+    <>
+      <Form/>
+    </>
+  )
+}
+
+export default App;
